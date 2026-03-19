b4a77e3022bddc61684efb00b4308f9e345f825c
Bhargavigaripalli <bhargavigaripalli@gmail.com>
2026-03-19 08:45:17 +0530

    Add files via upload
    upload

---
+++ task1.js
@@ -0,0 +1,189 @@
+var a=21
+console.log(a)
+  a=22
+  console.log(a)
+
+
+  let name= "Bhargavi"
+  console.log(name)
+  name="Garipalli"
+  console.log(name)
+
+
+  const DOB=17082002
+  console.log(DOB)
+  // DOB=112233
+  // console.log(DOB)
+  // here we cannot reassign the values. so,here we will get the type error.
+
+  var a=10
+  console.log(a)
+  var a=20
+  console.log(a)
+
+  let b="hello"
+   console.log(b)
+  // let b="bhargavi"
+  // console.log(b)
+  // here in let if redeclare then it will return the syntax error
+
+
+  console.log("Bhargavi Garipalli")
+  console.log(22)
+  console.log("kamareddy")
+  console.log(100)
+  console.log("JavaScript is fun")
+
+
+  // alert("Welcome to JavaScript")
+  // it shows the pop message that welcome to javascript
+
+
+//   let answer = confirm("Do you like coding?");
+// console.log(answer);
+// here firstly it gives do you like coding  if we give ok then it returns true and if we click cancel then it return false
+
+// let name1=prompt("What is your name?")
+// console.log(name1)
+// what the name will be given that will be shown in the console.
+
+
+console.warn("This is a warning")
+
+
+console.log("This is an error")
+
+
+// console.clear()
+// here it will clear the over all consoles.
+
+document.writeln("Hi frnds...!!!")
+
+// let name2 = prompt("Enter your name");
+// alert(name2);
+// console.log(name2);    here what ever the name we written  in prompt then return to alert our name and it console .
+
+
+// var age = prompt("Enter your age");
+// console.log(age);
+// document.writeln(age);    here what ever the age we written  then console and it return the websites
+
+// const breakfast = confirm("Have you had breakfast?");
+// console.log(breakfast);        
+//here firstly it gives do you like coding  if we give ok then it returns true and if we click cancel then it return false
+
+
+var city = "Mumbai"
+let country = "India"
+const planet = "Earth"
+console.log(city)
+console.log(country)
+console.log(planet)
+
+
+let c=5
+console.log(c)
+c=10
+console.log(c)
+c=15
+console.log(c)
+
+
+console.log("This is a normal message");
+console.warn("This is a warning message");
+console.error("This is an error message");
+
+
+// const name4 = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// var isStudent = confirm("Are you a student?");
+// console.log(name4);
+// console.log(age);
+// console.log(isStudent);
+// alert("Welcome " + name4);
+
+
+// console.clear();
+// alert("Hello!");
+// let name5 = prompt("What is your name?");
+// alert("Welcome " + name5);
+// document.writeln(name5);    firstly alert msg will trigger and then prompt then we need to answer it and we will get alert message
+
+
+
+// {
+// var x = 100
+// let y = 200
+// const z= 300
+// }
+// console.log(x)
+// console.log(y)
+// console.log(z) x will be print and y and z reference error it will return 
+
+
+var x = 10
+let y = 20
+const z= 30
+console.log(x)
+console.log(y)
+console.log(z)
+ 
+
+// let response = confirm("Do you like JavaScript?");
+// console.log(response);
+// console.warn(response);
+// console.error(response); here if confirm is ok then it will be true and if cancel then it returns false
+
+
+// const name5 = prompt("Enter your name");
+// document.writeln("Your name is: " + name5); here it will display in webpage 
+// console.log("Your name is: " + name5); it will console in output 
+
+
+
+// let age = prompt("Enter your age");
+// alert("Your age is: " + age); here it will give pop msg
+// console.log("Your age is: " + age);  it will give return in output
+// document.writeln("Your age is: " + age);  it will show in webpage
+
+
+// {
+//     var i = 10;
+// }
+// console.log(i);    it wil return output
+// {
+//     let j = 20;
+// }
+// console.log(j);    it shows reference error because console is outside  of the block  if it is in the block then it returns  output
+
+
+// let result = confirm("Are you 18+?");
+// console.log(result && "Adult");  if enter ok then it returns the adult 
+// console.warn(!result && "Minor");  if we enter cancel then it returns the minor .
+
+
+var fruit = "Apple";
+fruit = "Banana";
+let vehicle = "Car";
+vehicle = "Bike";
+console.log(fruit);
+console.log(vehicle);
+
+
+console.clear();
+alert("Practice Start");
+const name6 = prompt("Enter your name");
+console.log(name6);
+document.writeln(name6);
+
+
+
+
+
+
+
+
+
+
+
+ 
